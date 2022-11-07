f2ca75e49c277effee66ac954ef5541183a512a3
samz9n <samuel.langhoff@outlook.com>
2022-11-07 10:43:07 +0200

    working on listing specific querys questions

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,21 +1,35 @@
-import { Card, Box, CardContent, List, ListItem } from "@mui/material";
+import { Card, Box, CardContent, List, ListItem, Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 export const QuestionList = () => {
   const [questions, setQuestions] = useState([]);
+  const [queries, setQueries] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
   // Fetch all questions
   //CHANGE URL WHEN DEPLOYED TO HEROKU
   useEffect(() => {
     fetch("http://localhost:8080/questions")
-      .then((response) =>
-          response.json()
-          )
+      .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setQuestions(data)
+        setQuestions(data);
       })
-      .catch(err => console.log(err.message))
+      .catch((err) => console.log(err.message));
   }, []);
+  //FETCH all queries
+  useEffect(() => {
+    fetch("http://localhost:8080/queries")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQueries(data);
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
+
+  const handleClick = () => {
+    setIsClicked(true);
+  };
 
   return (
     <Box sx={{ display: "flex", justifyContent: "center" }}>
@@ -28,10 +42,37 @@ export const QuestionList = () => {
         }}
       >
         <CardContent>
+          {queries.map((query) => {
+            return (
+              <Button
+                key={query.queryId}
+                sx={{ margin: "4px" }}
+                variant="contained"
+                //TÄMÄ EI TOIMI VIELÄ
+                onClick={() => {
+                  return (
+                    <List>
+                      {query.questions.map((question) => {
+                        console.log(question);
+                        return (
+                          <ListItem key={question.questionId}>
+                            {question.title}
+                          </ListItem>
+                        );
+                      })}
+                    </List>
+                  );
+                }}
+              >
+                {query.title}
+              </Button>
+            );
+          })}
           <List>
-            {/* List all questions*/}
             {questions.map((question) => {
-              return <ListItem key={question.questionId}>{question.title}</ListItem>;
+              return (
+                <ListItem key={question.questionId}>{question.title}</ListItem>
+              );
             })}
           </List>
         </CardContent>
